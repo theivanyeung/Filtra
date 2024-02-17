58a3caeba47f8dd7d62b1490732c35e77bca1a0e
Jack Luo <jack145945@gmail.com>
2024-02-17 13:29:50 -0800

    backend: update image classifer and fix bugs

--- engine/api/app.py
+++ engine/api/app.py
@@ -5,9 +5,22 @@ from flask_cors import CORS
 import traceback
 import json
 import requests
+from transformers import ViltProcessor, ViltForQuestionAnswering
+from PIL import Image
+import io
+import base64
+import sys
+
+# Prompts
+sys.path.append("..")
+from components.prompts import text_analyze_prompt
 
 app = Flask(__name__)
 CORS(app)
+
+# Tranformer Models
+processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
 # Flask
 
@@ -31,10 +44,6 @@ def home():
     return "If humanity successfully builds AGI, how the hell are we gonna control it? In the meantime, I'm building this with the end goal of eventually helping me do shit while I'm operating my digital world. Starting out with an AI specializing in distraction management."
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-
 # Engine
 
 
@@ -55,7 +64,7 @@ def engine():
 
     print(json.dumps(content, indent=3))
 
-    return jsonify({"response": "something wrong happened"})
+    return jsonify({"response": "SHITTTT"})
 
 
 @app.route("/engine/text", methods=["POST"])
@@ -70,10 +79,76 @@ def text_classifier():
         messages=[
             {
                 "role": "system",
-                "content": "You are Filtra, an assistant focused on minimizing digital distractions by filtering out distractions within apps/websites instead of blocking them entirely, providing a personalized, adaptive, and productive digital experience and helping users maintain their focus",
+                "content": "You are Vesper, an assistant focused on minimizing digital distractions by filtering out distractions within apps/websites instead of blocking them entirely, providing a personalized, adaptive, and productive digital experience and helping users maintain their focus",
             },
             {"role": "user", "content": text_analyze_prompt + prompt},
         ],
     )
 
     return jsonify({"response": response.choices[0].message.content})
+
+
+@app.route("/engine/image", methods=["POST"])
+def image_classifier():
+    image_url = request.json.get("content")
+    isbase = request.json.get("isbase")
+
+    if not image_url:
+        return jsonify({"error": "No image URL uploaded"}), 400
+
+    if isbase:
+        # Decode the base64 data to get the image data
+        decoded_data = base64.b64decode(image_url)
+
+        # Convert the image data to an Image object
+        image = Image.open(io.BytesIO(decoded_data))
+    else:
+        image = Image.open(requests.get(image_url, stream=True).raw)
+
+    image = image.convert("RGB")
+
+    text = "Is this image so extremely distracting that the viewer will become addicted to it?"
+
+    encoding = processor(image, text, return_tensors="pt")
+
+    outputs = model(**encoding)
+    logits = outputs.logits
+    idx = logits.argmax(-1).item()
+
+    return jsonify({"response": model.config.id2label[idx]})
+
+
+@app.route("/engine/videos", methods=["POST"])
+def video_classifier():
+    video = request.json.get("video")
+
+    pass
+
+
+# Agent
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/entity", methods=["POST"])
+def entity():
+    data = request.json.get("data")
+
+    if not data:
+        return jsonify({"error": "Data not provided"}), 400
+
+    # response = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=[
+    #         {"role": "system", "content": "You are Vesper, an assistant focused on minimizing digital distractions by filtering out distractions within apps/websites instead of blocking them entirely, providing a personalized, adaptive, and productive digital experience and helping users maintain their focus"},
+    #         {"role": "user", "content": prompt},
+    #     ]
+    # )
+
+    # return jsonify({"response": response.choices[0].message.content})
+
+    return jsonify({"data": data})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
